693242e4f3f46d95f677d5fef058a1c0cc9ec8b2
Benjamin Wesson <benjamin.j.wesson@gmail.com>
2026-09-16 22:04:47 -0400

    testing routing and escape

--- hello.py
+++ hello.py
@@ -1,7 +1,14 @@
 from flask import Flask
-
+from markupsafe import escape
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    name = "World"
+    return f"<p>Hello, {escape(name)}</p>"
+
+@app.route("/bye")
+def goodbye_world():
+    name = "Goodbye"
+    return f"<p>Goodbye, {escape(name)}</p>"

6aee13f6ffd3f39da35ecb0f0a537589b3235e3c
Benjamin Wesson <benjamin.j.wesson@gmail.com>
2026-09-21 14:07:16 -0400

    better message display

--- model/messageModel.py
+++ model/messageModel.py
@@ -4,7 +4,7 @@ class messageModel():
      def get_message():
         conn = connector()
         cursor = conn.cursor()
-        cursor.execute("SELECT * FROM wesson")
+        cursor.execute("SELECT * FROM messages")
         messages = cursor.fetchall()
         cursor.close()
         conn.close()

10bbe29d06683012f19816bccca34123dc77616e
Benjamin Wesson <benjamin.j.wesson@gmail.com>
2026-09-22 13:53:45 -0400

    ai usage note

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
+#We used AI assitance to help rewrite our port number setup. 
+#Because we wanted to allow multiple group memebers to be on different ports and to have it be a true live server with the debug tag
+#This introduced a level of complexity that was above our knowledge level and it was not a core part of our delvierable.
+#We used AI for this as more of a quality of life feature.
+#We inputed our old set up createApp().run(debug=True, port=5001) as the prompt and asked for a way to have port by user input with persving the live server feature..
+
 import os
 from flask import Flask
 from controller.controller import viewHome
